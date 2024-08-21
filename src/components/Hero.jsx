0817181a6de05911c21505e3c0c3620c4c94d9d9
Your Name <sangameshshishir@gmail.com>
2024-08-21 21:15:19 +0530

    Updated profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/shishirProfile.jpg"
+import profilePic from "../assets/ShishirProfile.jpeg"
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
